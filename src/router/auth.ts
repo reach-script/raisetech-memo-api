3c0a1e269305b225051dfab29d343d1f9090971c
みずしま <mizuki.bz.3224@gmail.com>
2021-08-23 21:37:09 +0900

    fix: build

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,14 +1,13 @@
-import { json } from 'micro';
+import { json, send } from 'micro';
 import { post, router } from 'microrouter';
 import { sign } from 'jsonwebtoken';
 import { client } from '../libs/db/client';
-import { unAuthorizedError } from '../error';
 import dotenv from 'dotenv';
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export default router(
-  post('/login', async (req) => {
+  post('/login', async (req, res) => {
     const data = await json(req);
     const { email, password } = data;
 
@@ -16,12 +15,18 @@ export default router(
     const { data: users } = await client.from('user').select('*').eq('email', email);
 
     if (users.length === 0) {
-      return unAuthorizedError();
+      send(res, 401, {
+        message: 'unauthorized',
+      });
+      return;
     }
     const user = users[0];
 
     if (user.password !== password) {
-      return unAuthorizedError();
+      send(res, 401, {
+        message: 'unauthorized',
+      });
+      return;
     }
 
     const token = sign({ id: user.id }, JWT_SECRET, {
